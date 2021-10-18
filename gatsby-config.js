911391c099289633364dc402b82e423320d8f56a
luanpires94 <luan.94pires@gmail.com>
2021-10-18 17:01:36 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "list-card",
+    title: "site",
   },
   plugins: [
     "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-source-graphql",
       options: {
-        typeName: "alldata",
-        fieldName: "alldata",
-        url: "https://api-us-east-1.graphcms.com/v2/cku8rn0m642yc01yz16vd5076/master",
+        typeName: "",
+        fieldName: "",
+        url: "",
       },
     }
   ],

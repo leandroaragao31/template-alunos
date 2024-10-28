10793aea84971a6f1a64b72a7c99a7be7171688c
Leandro de Aragão Cezar <71722185+leandroaragao31@users.noreply.github.com>
2024-10-28 14:05:45 -0300

    Update styles.js

--- src/Componets/styles.js
+++ src/Componets/styles.js
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing:border-box;
-    background-image: url('https://media.graphassets.com/soz6jEh6Q8qUyll4J3l4');
+    background-image: url('https://w7.pngwing.com/pngs/262/252/png-transparent-bright-starry-sky-starry-sky-stars-universe-thumbnail.png');
     background-attachment: scroll;
     background-repeat:no-repeat;
     background-size:cover;
